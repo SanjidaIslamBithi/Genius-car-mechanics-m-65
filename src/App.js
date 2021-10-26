a7de783fce24fe6cf37ee311bc6e3c52627644a1
SanjidaIslamBithi <sanjida333cool@gmail.com>
2021-10-27 02:20:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -7,28 +7,35 @@ import Login from './Pages/Login/Login/Login';
 import Header from './Pages/Shared/Header/Header';
 import AuthProvider from './contexts/AuthProvider';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
-
+import AddService from './Pages/AddService/AddService';
+import ManageServices from './Pages/ManageServices/ManageServices';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <AuthProvider>
         <Router>
           <Header></Header>
           <Switch>
-            <Route exact path="/">
+            <Route exact path='/'>
               <Home></Home>
             </Route>
-            <Route path="/home">
+            <Route path='/home'>
               <Home></Home>
             </Route>
-            <Route path="/login">
+            <Route path='/login'>
               <Login></Login>
             </Route>
-            <PrivateRoute path="/booking/:serviceId">
+            <PrivateRoute path='/booking/:serviceId'>
               <Booking></Booking>
             </PrivateRoute>
-            <Route path="*">
+            <Route path='/addService'>
+              <AddService></AddService>
+            </Route>
+            <Route path='/manageServices'>
+              <ManageServices></ManageServices>
+            </Route>
+            <Route path='*'>
               <NotFound></NotFound>
             </Route>
           </Switch>
